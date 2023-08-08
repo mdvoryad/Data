a466c19e8f967248a058db6bdffdaa9683be5a0b
mdvoryad <99253433+mdvoryad@users.noreply.github.com>
2023-08-08 14:02:46 +0200

    Delete Data directory

--- Data/src/main/java/htw/kbe/repository/ComponentRepository.java
+++
@@ -1,7 +0,0 @@
-package htw.kbe.repository;
-
-import htw.kbe.model.Component;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ComponentRepository extends MongoRepository<Component, Integer> {
-}
--- Data/src/main/java/htw/kbe/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package htw.kbe.repository;
-
-import htw.kbe.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, Integer>{
-
-}
